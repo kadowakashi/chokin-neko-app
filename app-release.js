48630c45c51eb2e3c69b0f2ac2897e0e71c4c1f2
kadowakashi <rascal.ll.skytnk@gmail.com>
2026-09-18 10:45:41 +0900

    Release r20 cat room visual correction

--- app-release.js
+++ app-release.js
@@ -1,6 +1,6 @@
 (function (root) {
   'use strict';
-  const revision = 'r19';
+  const revision = 'r20';
   root.ChokinRelease = Object.freeze({
     revision,
     shellCache: `chokin-v100-shell-${revision}`
