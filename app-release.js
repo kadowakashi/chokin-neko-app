a119763e68fa322668665b9e098de13bfdf8652e
kadowakashi <rascal.ll.skytnk@gmail.com>
2026-09-23 15:44:57 +0900

    Release r22 hero banner

--- app-release.js
+++ app-release.js
@@ -1,6 +1,6 @@
 (function (root) {
   'use strict';
-  const revision = 'r21';
+  const revision = 'r22';
   root.ChokinRelease = Object.freeze({
     revision,
     shellCache: `chokin-v100-shell-${revision}`
